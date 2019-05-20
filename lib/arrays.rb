f26e2048c6e7ba632e3dcb8ad62839986ddf8c34
Maryam Joseph <josephmaryam.mj@gmail.com>
2019-05-20 10:45:17 -0400

    array review done

--- lib/arrays.rb
+++ lib/arrays.rb
@@ -1,12 +1,16 @@
-#Write your code here!
+require "pry"
 def using_max(my_array)
+  my_array.max
 end
 
 def find_nth_element(my_array)
+  my_array.flatten[6]
 end
 
 def flatten_nested_arrays(my_array)
+  my_array.flatten
 end
 
 def flatten_then_sort(my_array)
+  my_array.flatten.sort
 end
